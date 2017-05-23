9726b521a42a92e80d485a21543def344157e5d8
Pan Gaofeng <paeans@gmail.com>
2017-05-23 17:00:37 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+[[ $1 == "" ]] && msg="master update" || msg=$1
+
+remotelist="origin"
+branch="master"
+
+
+for remote in $remotelist
+do
+    git pull $remote $branch
+    [[ $? == 0 ]] || {
+        echo "ERROR between local and $remote"
+        echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        echo
+        exit 1
+    }
+    echo "**************************************"
+    echo
+done
+
+git add -A
+git commit -m "${msg}" && {
+#echo
+for remote in $remotelist
+do
+    echo
+    git push $remote $branch
+    #echo
+done } # || echo
+echo
