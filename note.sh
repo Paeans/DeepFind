d27f3db31583004c359c814a487baa0235518b81
Pan Gaofeng <paeans@gmail.com>
2017-05-23 19:42:45 +0800

    Update note.sh

--- note.sh
+++ note.sh
@@ -11,6 +11,3 @@ ${!array[@]}                                              #get all the indexes o
 
 grep -w "word"                                            #get the line match the specific word, no *word*
 grep -v "word"                                            #invert mode, get the line don't contain the word
-
-
-
